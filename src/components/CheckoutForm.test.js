1f9ee4d57bbdec086888e03099161a348034838c
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-04 11:18:19 -0600

    troubleshooting testing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -20,4 +20,20 @@ test("form shows success message on submit with form details", () => {
     const state = screen.getByLableText(/State/i);
     const zip = screen.getByLableText(/Zip/i);
     const submitButton = document.querySelector('button');
+
+    userEvent.type(firstName, 'Andrew');
+    userEvent.type(lastName, 'Busby');
+    userEvent.type(address, '123 some road');
+    userEvent.type(city, 'Provo');
+    userEvent.type(state, 'Utah');
+    userEvent.type(zip, '12345');
+    userEvent.click(submitButton);
+
+    const successMessage = document.querySelector("[data-testid='successMessage']");
+
+    expect(successMessage).toHaveTextContent(/Andrew Busby/);
+    expect(successMessage).toHaveTextContent(/123 some road/);
+    expect(successMessage).toHaveTextContent(/Prove/);
+    expect(successMessage).toHaveTextContent(/Utah/);
+    expect(successMessage).toHaveTextContent(/12345/);
 });
