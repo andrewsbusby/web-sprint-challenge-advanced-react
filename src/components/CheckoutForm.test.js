6f6137ad7139935449d23ce8e552e77b4bb4170b
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-04 11:04:33 -0600

    setting up second test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -9,4 +10,6 @@ test("form header renders", () => {
     screen.getByText('Checkout Form');
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render (<CheckoutForm/>)
+});
